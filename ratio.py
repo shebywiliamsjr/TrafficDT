8548a6a76ac8674d6dbd7a5255bf1e48c4e83ffd
Reeti Pradhananga <reetipd@gmail.com>
2025-02-24 11:48:46 -0600

    helper files

--- ratio.py
+++ ratio.py
@@ -1,37 +1,8 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# file_path = "YOLOv8_Vehicle_Detection.xlsx"  
-# df = pd.read_excel(file_path)
-
-# fps = 30
-
-# df = df.dropna(subset=['Frame', 'Hit_Ratio', 'Miss_Ratio'])
-# df['Frame'] = pd.to_numeric(df['Frame'], errors='coerce')
-
-# df['Second'] = (df['Frame'] // fps).astype(int)  
-
-# df_per_second = df.groupby('Second', as_index=False).agg({'Hit_Ratio': 'mean', 'Miss_Ratio': 'mean'})
-
-# plt.figure(figsize=(10, 5))
-# plt.plot(df_per_second['Second'], df_per_second['Hit_Ratio'], marker='o', linestyle='-', color='g', label="Hit Ratio")
-# plt.plot(df_per_second['Second'], df_per_second['Miss_Ratio'], marker='s', linestyle='--', color='r', label="Miss Ratio")
-
-# plt.xlabel("Time (Seconds)")
-# plt.ylabel("Ratio")
-# plt.title("Hit & Miss Ratio Per Second")
-# plt.legend()
-# plt.grid(True)
-
-# plt.show()
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import find_peaks
 
-# Load the Excel file
 file_path = "YOLO_Vehicle_Detection.xlsx" 
 df = pd.read_excel(file_path)
 
